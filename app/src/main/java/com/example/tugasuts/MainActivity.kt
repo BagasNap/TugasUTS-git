970ba4be86f6642e1ae7ebb23587b762caf03c9e
BagasNap <bagasagma.yogya@gmail.com>
2025-11-11 17:52:41 +0700

    Penyelesain UTS

--- app/src/main/java/com/example/tugasuts/MainActivity.kt
+++ app/src/main/java/com/example/tugasuts/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.tugasuts
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val username = findViewById<EditText>(R.id.editUsername)
+        val email = findViewById<EditText>(R.id.editEmail)
+        val firstName = findViewById<EditText>(R.id.editFirstName)
+        val lastName = findViewById<EditText>(R.id.editLastName)
+        val password = findViewById<EditText>(R.id.editPassword)
+        val confirmPassword = findViewById<EditText>(R.id.editConfirmPassword)
+        val btnKirim = findViewById<Button>(R.id.btnKirim)
+        val btnBatal = findViewById<Button>(R.id.btnBatal)
+
+        btnKirim.setOnClickListener {
+            val user = username.text.toString()
+            val mail = email.text.toString()
+            val first = firstName.text.toString()
+            val last = lastName.text.toString()
+            val pass = password.text.toString()
+            val confirm = confirmPassword.text.toString()
+
+            if (user.isEmpty() || mail.isEmpty() || first.isEmpty() || last.isEmpty() || pass.isEmpty() || confirm.isEmpty()) {
+                Toast.makeText(this, "Semua input harus diisi!", Toast.LENGTH_SHORT).show()
+            } else if (pass != confirm) {
+                Toast.makeText(this, "Password dan konfirmasi password tidak sama!", Toast.LENGTH_SHORT).show()
+            } else {
+                val fullName = "$first $last"
+                Snackbar.make(it, "User $fullName berhasil didaftarkan.", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        btnBatal.setOnClickListener {
+            username.text.clear()
+            email.text.clear()
+            firstName.text.clear()
+            lastName.text.clear()
+            password.text.clear()
+            confirmPassword.text.clear()
         }
     }
 }
